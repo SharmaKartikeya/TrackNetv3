7b6cd69b0cc43cee55d72583eed996ceaaef9165
SharmaKartikeya <kartikeya@quidich.com>
2023-07-14 20:48:21 +0530

    fixed wbce loss with positive weight

--- graphs/losses/wbce.py
+++ graphs/losses/wbce.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 class WBCE(nn.Module):
     def __init__(self, pos_factor: float):
         super(WBCE, self).__init__()
-        self.loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_factor))
+        pos_weight = torch.tensor(pos_factor, dtype=torch.float64)
+        self.loss = nn.BCEWithLogitsLoss(pos_weight)
 
     def forward(self, output, target):
         return self.loss(output, target)
